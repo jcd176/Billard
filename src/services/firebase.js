440bba21f5b23e6701278f0f6c165b0e9b5a3472
jcd176 <jcharles.douyere@gmail.com>
2026-05-31 12:30:20 +0200

    Update Firebase configuration with real credentials
    Updated Firebase configuration with actual API keys and IDs.

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,13 +5,13 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Remplacement direct des valeurs pour éliminer les bugs de configuration Vercel
 const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY_RECUPEREE_SUR_FIREBASE", 
+  apiKey: "AIzaSyC5I_5lHxPH57-rmN_br2SPcUCbmmSti-U", 
   authDomain: "billard-17260.firebaseapp.com",
   projectId: "billard-17260",
   storageBucket: "billard-17260.appspot.com",
-  messagingSenderId: "VOTRE_MESSAGING_SENDER_ID_RECUPERE_SUR_FIREBASE",
-  appId: "VOTRE_APP_ID_RECUPERE_SUR_FIREBASE",
-  measurementId: "VOTRE_MEASUREMENT_ID_RECUPERE_SUR_FIREBASE",
+  messagingSenderId: "247215612831",
+  appId: "1:247215612831:web:70c5b7b6cd642f09a213aa",
+  measurementId: "G-3PPC69JKMM",
   // URL absolue impérative pour cibler la bonne zone géographique (Europe)
   databaseURL: "https://billard-17260-default-rtdb.europe-west1.firebasedatabaseapp.com"
 };
